e41bbf4d107a34ebbea8498f3524d97dc2cf4fac
Роман Фисенко <1roman.fis@gmail.com>
2025-09-04 19:52:08 +0700

    Перенаправление

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, redirect
 import datetime
 app = Flask(__name__)
 
@@ -60,3 +60,7 @@ def counter():
                 Ваш IP-адрес: ''' + client_ip + '''<br>
            </body>
         </html>'''
+
+@app.route("/info")
+def info():
+    return redirect("/author")
