d173bb39f9b97b1aa0043175726aa74f94f007d7
Студент Факультета Бизнеса <student@FB.LOCAL>
2025-09-17 16:58:04 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, redirect
+from flask import Flask, url_for, request, redirect, abort
 import datetime
 app = Flask(__name__)
 
@@ -250,3 +250,12 @@ def a():
 @app.route('/lab2/a/')
 def a2():
     return 'Слэш есть'
+
+flower_list = ('Роза', 'Тюльпан', 'Незабудка', 'Ромашка')
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len(flower_list):
+        abort(404)
+    else:
+        return 'Цветочек: ' + flower_list[flower_id]
