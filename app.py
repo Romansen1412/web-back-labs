5bee1c81c17f57ad9faa4bf561c0294093d41b87
Роман Фисенко <1roman.fis@gmail.com>
2025-09-30 19:56:28 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -287,3 +287,7 @@ def example():
         {'name': 'манго', 'price': 250}
     ]
     return render_template('example.html', name=name, lab=lab, group=group, course=course, fruits=fruits)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
