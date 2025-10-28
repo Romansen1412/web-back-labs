a53de8c91fda04394d8fd25a03fc522fc0a573b0
Роман Фисенко <1roman.fis@gmail.com>
2025-10-28 23:26:28 +0700

    холодильник

--- lab4.py
+++ lab4.py
@@ -145,3 +145,36 @@ def login():
 def logout():
     session.pop('login', None)
     return redirect('/lab4/login')
+
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge():
+    message = ''
+    snowflakes = 0
+    temperature = None
+
+    if request.method == 'POST':
+        temp_input = request.form.get('temperature')
+
+        if not temp_input:
+            message = 'Ошибка: не задана температура'
+        else:
+            try:
+                temperature = float(temp_input)
+
+                if temperature < -12:
+                    message = 'Не удалось установить температуру — слишком низкое значение'
+                elif temperature > -1:
+                    message = 'Не удалось установить температуру — слишком высокое значение'
+                elif -12 <= temperature <= -9:
+                    message = f'Установлена температура: {temperature}°С'
+                    snowflakes = 3
+                elif -8 <= temperature <= -5:
+                    message = f'Установлена температура: {temperature}°С'
+                    snowflakes = 2
+                elif -4 <= temperature <= -1:
+                    message = f'Установлена температура: {temperature}°С'
+                    snowflakes = 1
+            except ValueError:
+                message = 'Ошибка: введено некорректное значение'
+
+    return render_template('/lab4/fridge.html', message=message, snowflakes=snowflakes, temperature=temperature)
