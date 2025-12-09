d2a57c5cad1c0b5fca3693a620dd8daaa0da7bc5
Роман Фисенко <1roman.fis@gmail.com>
2025-12-09 18:58:01 +0700

    Вывод таблицы

---
+++ static/lab7/main.js
@@ -0,0 +1,39 @@
+function fillFilmlist() {
+    fetch('/lab7/rest-api/films/')
+        .then(function(data) {
+            return data.json();
+        })
+        .then(function(films) {
+            let tbody = document.getElementById('film-list');
+            tbody.innerHTML = '';
+
+            for (let i = 0; i < films.length; i++) {
+                let tr = document.createElement('tr');
+
+                let tdTitleRus = document.createElement('td');
+                let tdTitle = document.createElement('td');
+                let tdYear = document.createElement('td');
+                let tdAction = document.createElement('td');
+
+                tdTitle.innerText = films[i].title == films[i].title_ru ? '' : films[i].title; 
+                tdTitleRus.innerText = films[i].title_ru;
+                tdYear.innerText = films[i].year;
+
+                let editButton = document.createElement('button');
+                editButton.innerText = 'редактировать';
+                
+                let delButton = document.createElement('button');
+                delButton.innerText = 'удалить';
+
+                tdAction.append(editButton);
+                tdAction.append(delButton);
+
+                tr.append(tdTitle);
+                tr.append(tdTitleRus);
+                tr.append(tdYear);
+                tr.append(tdAction);
+
+                tbody.append(tr);
+            }
+        })
+}
